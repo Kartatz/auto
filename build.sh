ff2f5d00b561d414c493a1674d665c911669c6a2
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-06-21 23:42:15 -0300

    rebase

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INSTALL_PREFIX=/tmp/autotools
+
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz
+tar xf autoconf-*.tar.xz
+cd autoconf-*
+
+./configure --prefix="${INSTALL_PREFIX}" --host="${CROSS_COMPILE_TRIPLET}"
+make
+make install
+
+wget https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.xz
+tar xf automake-*.tar.xz
+cd automake-*
+
+./configure --prefix="${INSTALL_PREFIX}" --host="${CROSS_COMPILE_TRIPLET}"
+make
+make install
